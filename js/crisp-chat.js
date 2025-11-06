e6c0241963953ca7b3aa2adb0385961ea6940076
TheConnorGamer <105085284+TheConnorGamer@users.noreply.github.com>
2025-11-06 22:23:48 +0000

    fix: add Crisp chat to all pages with external script file

---
+++ js/crisp-chat.js
@@ -0,0 +1,11 @@
+// Crisp Chat Integration
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "99ea6bb5-9995-4cb4-bf3d-298eefa5b4f0";
+
+(function() {
+    var d = document;
+    var s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+})();
